45c313871d30d439d5a3e9181588fcfa8e8f66d0
deepgunner <deepindersingh.23@gmail.com>
2018-02-02 18:28:08 +0530

    release 0

---
+++ Weather.js
@@ -0,0 +1,18 @@
+var request = require('./request');
+var city = 'Thiruvananthapuram'
+
+var url = 'https://query.yahooapis.com/v1/public/yql?q=select%20astronomy%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22'+ city +'%2C%20%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys'
+request(url, function (error, response, body) {
+   
+  if(!error && response.statusCode == 200){
+    console.log(body); // Print the HTML for the Google homepage.
+    var data = JSON.parse(body);
+    console.log('Sunrise and Sunset timings in ' + city + ':\nSunrise :'+ data.query.results.channel.astronomy.sunrise+'\nSunset :'+data.query.results.channel.astronomy.sunset);
+  }
+  
+  
+});
+
+
+
+
