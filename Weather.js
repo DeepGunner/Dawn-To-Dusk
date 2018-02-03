c516142297aa9890df61ae84bb208365e9a5aabd
School of Accelerated Learning <soal@Schools-Mac-mini.local>
2018-02-03 15:02:15 +0530

    update with ajax

--- Weather.js
+++ Weather.js
@@ -1,8 +1,29 @@
 //var request = require('request');
-jQuery(document).ready(function($){
-     var city = 'Delhi';
+function makeAjaxCall(url, methodType){
+   var xhr = new XMLHttpRequest();
+   xhr.open(methodType, url, true);
+   xhr.send();
+   xhr.onreadystatechange = function(){
+     if (xhr.readyState === 4){
+        if (xhr.status === 200){
+           console.log("xhr done successfully");
+           var resp = xhr.responseText;
+           var respJson = JSON.parse(resp);
+        } else {
+          console.log("xhr failed");
+        }
+     } else {
+        console.log("xhr processing going on");
+     }
+  }
+  console.log("request sent succesfully");
+}
+
+var city = 'Delhi';
 
 var url = 'https://query.yahooapis.com/v1/public/yql?q=select%20astronomy%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22'+ city +'%2C%20%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys'
+makeAjaxCall(url, "GET");
+
 // request(url, function (error, response, body) {
    
 //   if(!error && response.statusCode == 200){
@@ -13,21 +34,7 @@ var url = 'https://query.yahooapis.com/v1/public/yql?q=select%20astronomy%20from
   
   
 // });
-$.ajax({
-  url: url,
-  type: 'GET',
-  dataType: 'json',
-  //data: params,
-  success: function(response) {
-    return response.access_token;
-  },
-  error: function(error) {
-    return 'ERROR in webdamAuthenticate';
-  }
-});
-//your stuff
-});
-
+ 
 
 
 
