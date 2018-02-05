f896f507b926a27c661312ea1995ab5b78ff360c
deepgunner <deepindersingh.23@gmail.com>
2018-02-05 19:08:53 +0530

    final release

--- Weather.js
+++ Weather.js
@@ -8,7 +8,9 @@ $(function(){
 
 $('#inpt_search').keyup(function(e){
     if(e.keyCode === 13 ){
+
     var city = $("#inpt_search").val();
+    $("#display").text(city);
     $("#inpt_search").val(' ');
     e.preventDefault();
     var url = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22'+ city +'%2C%20%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys';
